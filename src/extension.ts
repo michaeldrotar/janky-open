7ea7278cdb7b3218ceee3807fe13c4e86ee89390
Michael <michaeldrotar@gmail.com>
2020-11-11 07:55:24 -0500

    Add workspace API to POC

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,24 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("jankyOpen.openPrompt", async () => {
+      const workspace = vscode.workspace;
+      const fs = workspace.fs;
+      const textDocuments = workspace.textDocuments;
       const quickPick = vscode.window.createQuickPick();
       quickPick.items = [
+        { label: vscode.workspace.name || "" },
+        {
+          label: (vscode.workspace.workspaceFolders || [])
+            .map((folder) => folder.uri.fsPath)
+            .join(","),
+        },
         { label: "one", foo: { bar: 1 } } as vscode.QuickPickItem,
         { label: "two" },
-      ];
+      ].concat(
+        textDocuments.map((doc) => {
+          return { label: doc.fileName };
+        })
+      );
       quickPick.onDidChangeValue((value) => {
         console.log(value);
       });
